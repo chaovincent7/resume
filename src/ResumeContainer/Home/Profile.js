e75f85b5c4bed17ce5a68223287aaf38e3c8826e
Vincent Chao <chaovincent7@gmail.com>
2022-07-10 17:23:50 -0500

    Created files and started on Profile

---
+++ src/ResumeContainer/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-data'>
+                <div className='colz'>
+                    <a href='#'>
+                        <i className='fa fa-instagram-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-github-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-linkedin-square'></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
